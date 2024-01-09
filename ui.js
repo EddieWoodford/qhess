438f3ea36c8839b8ae45bd34d084639efcf6cf84
Nemo Thorx <nemo@thorx.net>
2024-01-10 00:04:33 +1000

    header now flex. beginning of modal popups

---
+++ ui.js
@@ -0,0 +1,11 @@
+var hd = document.getElementById('helpDiv'),
+hb = document.getElementById('helpButton'),
+hc = document.getElementById('helpClose');
+
+function displayHelp() {
+  hd.style.display = 'block'
+};
+function closeHelp() {
+  hd.style.display = 'none'
+};
+
