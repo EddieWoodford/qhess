9b622944348b6d09f4f35592851ba530bf92df31
Nemo Thorx <nemo@thorx.net>
2024-01-10 13:21:42 +1000

    four modals now in place, and game metadata moved into one

--- ui.js
+++ ui.js
@@ -1,11 +1,52 @@
-var hd = document.getElementById('helpDiv'),
+function doNothing() {
+  event.preventDefault();
+  event.stopPropagation();
+};
+
+// infomation modal
+var iw = document.getElementById('infoWrapper'),
+ib = document.getElementById('infoButton'),
+ic = document.getElementById('infoClose');
+
+function displayInfo() {
+  iw.style.display = 'block'
+};
+function closeInfo() {
+  iw.style.display = 'none'
+};
+
+// help modal
+var hw = document.getElementById('helpWrapper'),
 hb = document.getElementById('helpButton'),
 hc = document.getElementById('helpClose');
 
 function displayHelp() {
-  hd.style.display = 'block'
+  hw.style.display = 'block'
 };
 function closeHelp() {
-  hd.style.display = 'none'
+  hw.style.display = 'none'
 };
 
+// meta (score and share) modal
+var mw = document.getElementById('metaWrapper'),
+mb = document.getElementById('metaButton'),
+mc = document.getElementById('metaClose');
+
+function displayMeta() {
+  mw.style.display = 'block'
+};
+function closeMeta() {
+  mw.style.display = 'none'
+};
+
+// options modal
+var ow = document.getElementById('optionsWrapper'),
+ob = document.getElementById('optionsButton'),
+oc = document.getElementById('optionsClose');
+
+function displayOptions() {
+  ow.style.display = 'block'
+};
+function closeOptions() {
+  ow.style.display = 'none'
+};
